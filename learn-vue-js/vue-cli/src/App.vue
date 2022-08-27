d8fe0675738d31cb72ea58840a739e0263bd52bf
taemin-Jang <jtm990506@gmail.com>
2022-08-27 22:43:06 +0900

    vue-cli 이해 및 실습

---
+++ learn-vue-js/vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <AppHeader v-bind:propsdata="str" v-on:renew="renewSting" />
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      str: "Header",
+    };
+  },
+  methods: {
+    renewSting() {
+      this.str = "hi";
+    },
+  },
+};
+</script>
+
+<style></style>
