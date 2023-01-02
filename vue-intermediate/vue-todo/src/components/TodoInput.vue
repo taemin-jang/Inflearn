966ac22a88b9d110f28c6cc39d7831eaad5e6418
taemin-Jang <jtm990506@gmail.com>
2023-01-02 23:30:54 +0900

    Todo App 컴포넌트 설계

---
+++ vue-intermediate/vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <span class="addContainer">
+      <i class="fa-solid fa-plus addBtn" v-on:click="addTodo"></i>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      // 로컬스토리지에 저장
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+
+.inputBox {
+  background-color: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478f8, #8763f8);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
---
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" :key="todoItem" class="shadow">
+        {{ todoItem }}
+        <span class="removeBtn" v-on:click="removeTodo(todoItem)">
+          <i class="fa-solid fa-trash-can"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todoItems: [],
+    };
+  },
+  methods: {
+    removeTodo(key) {
+      localStorage.removeItem(key);
+      this.todoItems.splice(this.todoItems.indexOf(key), 1);
+    },
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+      console.log(this.todoItems);
+    }
+  },
+};
+</script>
+
+<style>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted {
+  color: #b3adad;
+}
+
+.textCompleted {
+  text-decoration: line-through;
+  color: b3adad;
+}
+</style>
