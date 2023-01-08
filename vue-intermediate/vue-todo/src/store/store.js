cb0680f29e25b028abd8c92218d9861fafd7e4a4
taemin-Jang <jtm990506@gmail.com>
2023-01-09 00:58:26 +0900

    [리펙토링] store 속성 모듈화 getters, mutations

--- vue-intermediate/vue-todo/src/store/store.js
+++ vue-intermediate/vue-todo/src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import * as getters from "./getters";
+import * as mutations from "./mutations";
 Vue.use(Vuex);
 
 const storage = {
@@ -19,38 +20,6 @@ export const store = new Vuex.Store({
   state: {
     todoItems: storage.fetch(),
   },
-  getters: {
-    storedTodoItems(state) {
-      return state.todoItems;
-    },
-  },
-  mutations: {
-    addOneItem(state, todoItem) {
-      // 완료 했는지 상태 여부 JSON으로 저장
-      let obj = { completed: false, item: todoItem };
-      // 로컬스토리지에 저장
-      localStorage.setItem(todoItem, JSON.stringify(obj));
-      // todoItems에 추가해줌으로써 새로고침하지 않고도 최신 데이터가 반영
-      state.todoItems.push(obj);
-    },
-    removeOneItem(state, payload) {
-      localStorage.removeItem(payload.todoItem.item);
-      state.todoItems.splice(payload.index, 1);
-    },
-    toggleOneItem(state, index) {
-      state.todoItems[index].completed = !state.todoItems[index].completed;
-      // localStorage에는 update 기능이 없어서 삭제 후 등록을 해줘야함
-      localStorage.removeItem(state.todoItems[index].item);
-      localStorage.setItem(
-        state.todoItems[index].item,
-        JSON.stringify(state.todoItems[index])
-      );
-    },
-    clearAllItems(state) {
-      // 로컬스토리지 초기화
-      localStorage.clear();
-      // todoItems 초기화
-      state.todoItems = [];
-    },
-  },
+  getters, // getters: getters,
+  mutations, // mutations: mutations,
 });
